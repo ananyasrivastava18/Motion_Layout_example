643ae1355fd9cda6dd369db110d2583285a16446
Ananya <ananyasrivastava02872@gmail.com>
2022-07-23 17:00:39 +0530

    Initial commit

---
+++ app/src/main/java/com/digipod/motion_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.digipod.motion_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity2)
+    }
+}
